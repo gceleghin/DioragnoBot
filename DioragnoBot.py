257d6e30e5ef2b42da334c1c046c78cf3ddb9ef1
Giordano Celeghin <giordano.celeghin@gmail.com>
2023-02-07 18:12:13 +0100

    First tests: start, caps, echo and inline

--- DioragnoBot.py
+++ DioragnoBot.py
@@ -1,11 +1,55 @@
 #!/usr/bin/env python3
 
-import token
-from telegram import Update
-from telegram.ext import Updater
+import api_token
 
-def main():
-	updater = Updater(hidden.token)
+import logging
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, InlineQueryHandler
+
+logging.basicConfig(
+	format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+	level = logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await context.bot.send_message(
+		chat_id = update.effective_chat.id,
+		text = "FFFFFFFUUUUUUCK!"
+	)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	text_caps = ' '.join(context.args).upper()
+	await context.bot.send_message(chat_id = update.effective_chat.id, text = text_caps)
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	query = update.inline_query.query
+	if not query:
+		return
+	results = []
+	results.append(
+		InlineQueryResultArticle(
+			id = query.upper(),
+			title = 'Caps',
+			input_message_content = InputTextMessageContent(query.upper())
+		)
+	)
+	await context.bot.answer_inline_query(update.inline_query.id, results)
 
 if __name__ == '__main__':
-	main()
+	application = ApplicationBuilder().token(api_token.token).build()
+
+	start_handler = CommandHandler('start', start)
+	caps_handler = CommandHandler('caps', caps)
+	echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+	inline_caps_handler = InlineQueryHandler(inline_caps)
+
+	application.add_handler(start_handler)
+	application.add_handler(caps_handler)
+	application.add_handler(echo_handler)
+	application.add_handler(inline_caps_handler)
+
+	application.run_polling()
